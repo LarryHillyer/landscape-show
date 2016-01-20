102e2638a531cbe548933d345576fb2583dded49
LarryHillyer <LarryHillyer@hotmail.com>
2016-01-19 18:24:33 -0700

    Adding profile images to directory

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -1,3 +1,116 @@
+var slideShowElem;
+var slideListElem;
+var slideTabsElem;
+
+function Slide(src, alt) {
+    this.src=src;
+    this.alt=alt;
+}
+
+var slide0 = new Slide("assets/img/IMG_0588.JPG", "slide 0");
+var slide1 = new Slide("assets/img/IMG_1130.JPG", "slide 1");
+var slide2 = new Slide("assets/img/IMG_1180.JPG", "slide 2")
+var slide3 = new Slide("assets/img/IMG_1261.JPG", "slide 3")
+var slide4 = new Slide("assets/img/IMG_1314.JPG", "slide 4");
+var slide5 = new Slide("assets/img/IMG_1325.JPG", "slide 5");
+var slide6 = new Slide("assets/img/IMG_1351.JPG", "slide 6");
+var slide7 = new Slide("assets/img/IMG_1633.JPG", "slide 7");
+var slide8 = new Slide("assets/img/IMG_1670.JPG", "slide 8");
+var slide9 = new Slide("assets/img/IMG_1701.JPG", "slide 9");
+var slide10 = new Slide("assets/img/IMG_1771.JPG", "slide 10");
+var slide11 = new Slide("assets/img/IMG_1862.JPG", "slide 11");
+var slide12 = new Slide("assets/img/IMG_1968.JPG", "slide 12");
+var slide13 = new Slide("assets/img/IMG_2044.JPG", "slide 13");
+var slide14 = new Slide("assets/img/IMG_2051.JPG", "slide 14");
+var slide15 = new Slide("assets/img/IMG_2070.JPG", "slide 15");
+var slide16 = new Slide("assets/img/IMG_2087.JPG", "slide 16");
+var slide17 = new Slide("assets/img/IMG_2088.JPG", "slide 17");
+var slide18 = new Slide("assets/img/IMG_3191.JPG", "slide 18");
+var slide19 = new Slide("assets/img/IMG_3245.JPG", "slide 19");
+var slide20 = new Slide("assets/img/IMG_3251.JPG", "slide 20");
+var slide21 = new Slide("assets/img/IMG_3264.JPG", "slide 21");
+var slide22 = new Slide("assets/img/IMG_3280.JPG", "slide 22");
+var slide23 = new Slide("assets/img/IMG_3300.JPG", "slide 23");
+var slide24 = new Slide("assets/img/IMG_4022.JPG", "slide 24")
+var slide25 = new Slide("assets/img/IMG_4037.JPG", "slide 25")
+var slide26 = new Slide("assets/img/IMG_4058.JPG", "slide 26");
+var slide27 = new Slide("assets/img/IMG_4093.JPG", "slide 27");
+var slide28 = new Slide("assets/img/IMG_4100.JPG", "slide 28");
+var slide29 = new Slide("assets/img/IMG_4430.JPG", "slide 29");
+var slide30 = new Slide("assets/img/IMG_4511.JPG", "slide 30");
+var slide31 = new Slide("assets/img/IMG_4578.JPG", "slide 31");
+var slide32 = new Slide("assets/img/IMG_4590.JPG", "slide 32");
+var slide33 = new Slide("assets/img/IMG_4676JPG", "slide 33");
+var slide34 = new Slide("assets/img/IMG_4784.JPG", "slide 34");
+var slide35 = new Slide("assets/img/IMG_4907.JPG", "slide 35");
+var slide36 = new Slide("assets/img/P1000023(1).JPG", "slide 36");
+var slide37 = new Slide("assets/img/P1000069(1).JPG", "slide 37");
+var slide38 = new Slide("assets/img/P1000135(1).JPG", "slide 38");
+var slide39 = new Slide("assets/img/P1000144(1).JPG", "slide 39");
+var slide40 = new Slide("assets/img/P1000149(1).JPG", "slide 40");
+var slide41 = new Slide("assets/img/P1000152(1).JPG", "slide 41");
+var slide42 = new Slide("assets/img/P1000154(1).JPG", "slide 42");
+var slide43 = new Slide("assets/img/P1000155(1).JPG", "slide 43");
+var slide44 = new Slide("assets/img/P1000156(1).JPG", "slide 44");
+var slide45 = new Slide("assets/img/P1000157(1).JPG", "slide 45");
+var slide46 = new Slide("assets/img/P1000167(1).JPG", "slide 46");
+var slide47 = new Slide("assets/img/P1000403(1).JPG", "slide 47");
+var slide48 = new Slide("assets/img/P1000449(1).JPG", "slide 48");
+var slide49 = new Slide("assets/img/P1000693(1).JPG", "slide 49");
+
+
+var landscapeSlides = [];
+landscapeSlides.push(slide0);
+landscapeSlides.push(slide1);
+landscapeSlides.push(slide2);
+landscapeSlides.push(slide3);
+landscapeSlides.push(slide4);
+landscapeSlides.push(slide5);
+landscapeSlides.push(slide6);
+landscapeSlides.push(slide7);
+landscapeSlides.push(slide8);
+landscapeSlides.push(slide9);
+landscapeSlides.push(slide10);
+landscapeSlides.push(slide11);
+landscapeSlides.push(slide12);
+landscapeSlides.push(slide13);
+landscapeSlides.push(slide14);
+landscapeSlides.push(slide15);
+landscapeSlides.push(slide16);
+landscapeSlides.push(slide17);
+landscapeSlides.push(slide18);
+landscapeSlides.push(slide19);
+landscapeSlides.push(slide20);
+landscapeSlides.push(slide21);
+landscapeSlides.push(slide22);
+landscapeSlides.push(slide23);
+landscapeSlides.push(slide24);
+landscapeSlides.push(slide25);
+landscapeSlides.push(slide26);
+landscapeSlides.push(slide27);
+landscapeSlides.push(slide28);
+landscapeSlides.push(slide29);
+landscapeSlides.push(slide30);
+landscapeSlides.push(slide31);
+landscapeSlides.push(slide32);
+landscapeSlides.push(slide33);
+landscapeSlides.push(slide34);
+landscapeSlides.push(slide35);
+landscapeSlides.push(slide36);
+landscapeSlides.push(slide37);
+landscapeSlides.push(slide38);
+landscapeSlides.push(slide39);
+landscapeSlides.push(slide40);
+landscapeSlides.push(slide41);
+landscapeSlides.push(slide42);
+landscapeSlides.push(slide43);
+landscapeSlides.push(slide44);
+landscapeSlides.push(slide45);
+landscapeSlides.push(slide46);
+landscapeSlides.push(slide47);
+landscapeSlides.push(slide48);
+landscapeSlides.push(slide49);
+
 $(document).ready(function() {
 
     $(".rslides").responsiveSlides({maxwidth:650});   
